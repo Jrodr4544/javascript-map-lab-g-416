e3cf50800838a40c90659bad1df6ebe45ef59c4f
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-07-20 22:10:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const zebraStripes = [
 
 const sortedRobots = robots.map( robot => {
   let isDecepticon = knownDecepticons.includes(robot.name);
-  return Object.assign(results, robot, {
+  return Object.assign(robot, {
        name: robot.name,
        alliance: isDecepticon ? 'decepticon' : 'autobot'
   })
