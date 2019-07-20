7906d73ae2aed3133d24ddaf81438d2284170b0b
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-07-20 22:10:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const sortedRobots = robots.map( robot => {
   let isDecepticon? = knownDecepticons.includes(robot.name);
   return Object.assign(results, robot, {
        name: robot.name,
-       alliance: isDecpticon? 'decepticon' : 'autobot'
+       alliance: isDecepticon? 'decepticon' : 'autobot'
   })
 })
 
